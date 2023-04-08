11b9b4157f04b3d5c4f28dc2921cfadc1cc4aac2
Janit Bidhan <janitbidhan@gmail.com>
2023-04-08 03:29:48 -0400

    SPRINGBOOT BASE SETUP

---
+++ backend/src/test/java/tech/bittales/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bittales.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
